2cb104919a0f91554de76fc8d4a03626cdded598
wein <106440069+spacexrandom@users.noreply.github.com>
2023-02-03 13:19:15 +0300

    Delete Blacklist.lua

--- Scripts/Blacklist.lua
+++
@@ -1,7 +0,0 @@
-blacklisted = {
-704597875, 3656041166, 4208028829, 2329184113,
-}
-
-if table.find(blacklisted, game:GetService("Players").LocalPlayer.UserId) then
-    game:GetService("Players").LocalPlayer:Kick("Blacklisted"); game:Shutdown();
-end
