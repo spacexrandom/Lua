63bf2af92a2a6d7f21d7d7098f247d42f8ba263c
wein <106440069+spacexrandom@users.noreply.github.com>
2023-08-18 16:23:22 +0300

    Add files via upload

---
+++ Modules/Database.lua
@@ -0,0 +1,27 @@
+local Database = {
+    Whitelist = {
+        12345,
+    },
+
+    Blacklist = {
+        12345,
+    },
+
+    Announcement = {
+        "No announcement has been made!",
+    },
+
+    Update = {
+        "No update has been made!",
+    },
+
+    Credit = {
+        "0neHeartWein ・ luf1an (Discord)",
+    },
+
+    Invite = {
+        "xwvVT3a4ft",
+    },
+}
+
+return Database
